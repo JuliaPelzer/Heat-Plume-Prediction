e1b2be515abc34fe81d85fba1e5d02ae67cfc8be
ac130513 <julia.pelzer@ipvs.uni-stuttgart.de>
2022-07-20 14:28:33 +0200

    dataloader

--- data/dataloader.py
+++ data/dataloader.py
@@ -8,6 +8,7 @@ class DataLoader:
     Dataloader Class
     Defines an iterable batch-sampler over a given dataset
     """
+
     def __init__(self, dataset, batch_size=1, shuffle=False, drop_last=False):
         """
         :param dataset: dataset from which to load the data
@@ -24,25 +25,14 @@ class DataLoader:
         self.drop_last = drop_last
 
     def __iter__(self):
-        ########################################################################
-        # Define an iterable function that samples batches from the dataset.   #
-        # Each batch should be a dict containing numpy arrays of length        #
-        # batch_size (except for the last batch if drop_last=True)             #
-        # Hints:                                                               #
-        #   - np.random.permutation(n) can be used to get a list of all        #
-        #     numbers from 0 to n-1 in a random order                          #
-        #   - To load data efficiently, you should try to load only those      #
-        #     samples from the dataset that are needed for the current batch.  #
-        #     An easy way to do this is to build a generator with the yield    #
-        #     keyword, see https://wiki.python.org/moin/Generators             #
-        #   - Have a look at the "DataLoader" notebook first. This function is #
-        #     supposed to combine the functions:                               #
-        #       - combine_batch_dicts                                          #
-        #       - batch_to_numpy                                               #
-        #       - build_batch_iterator                                         #
-        #     in section 1 of the notebook.                                    #
-        ########################################################################
         def combine_batch_dicts(batch):
+            """
+            Combines a given batch (list of dicts) to a dict of numpy arrays
+            :param batch: batch, list of dicts
+                e.g. [{k1: v1, k2: v2, ...}, {k1:, v3, k2: v4, ...}, ...]
+            :returns: dict of numpy arrays
+                e.g. {k1: [v1, v3, ...], k2: [v2, v4, ...], ...}
+            """
             batch_dict = {}
             for data_dict in batch:
                 for key, value in data_dict.items():
@@ -52,40 +42,33 @@ class DataLoader:
             return batch_dict
 
         def batch_to_numpy(batch):
+            """Transform all values of the given batch dict to numpy arrays"""
             numpy_batch = {}
             for key, value in batch.items():
                 numpy_batch[key] = np.array(value)
-            return numpy_batch      
+            return numpy_batch
 
         if self.shuffle:
             index_iterator = iter(np.random.permutation(len(self.dataset)))
         else:
-            index_iterator = iter(np.range(len(self.dataset)))
+            index_iterator = iter(range(len(self.dataset)))
 
         batch = []
-        for index in index_iterator:  # iterate over indices using the iterator
-                batch.append(self.dataset[index])
-                if len(batch) == self.batch_size:
-                    yield batch_to_numpy(combine_batch_dicts(batch))  # use yield keyword to define a iterable generator
-                    batch = []
+        for index in index_iterator:
+            batch.append(self.dataset[index])
+            if len(batch) == self.batch_size:
+                yield batch_to_numpy(combine_batch_dicts(batch))
+                batch = []
 
-        if not self.drop_last and len(batch) > 0:
+        if len(batch) > 0 and not self.drop_last:
             yield batch_to_numpy(combine_batch_dicts(batch))
+
 
     def __len__(self):
         length = None
-        ########################################################################
-        # Return the length of the dataloader                                  #
-        # this is the number of batches you can sample from the dataset.       #  
-        ########################################################################
         if self.drop_last:
             length = len(self.dataset) // self.batch_size
-        elif len(self.dataset) % self.batch_size == 0:
-            length = len(self.dataset) // self.batch_size
         else:
-            length = len(self.dataset) // self.batch_size + 1
+            length = int(np.ceil(len(self.dataset) / self.batch_size))
 
-        ########################################################################
-        #                           END OF YOUR CODE                           #
-        ########################################################################
         return length
