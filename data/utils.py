96600d66c4b592cd3c03cbc82023183ac2ebf362
ac130513 <julia.pelzer@ipvs.uni-stuttgart.de>
2022-10-12 14:23:06 +0200

    [WIP] utils: new class Location instead of dataset_name, dataset_path with check_path function

--- data/utils.py
+++ data/utils.py
@@ -1,5 +1,16 @@
 import pickle
 import os
+# from dataclasses import dataclass
+
+# @dataclass
+# class Location():
+#     name:str
+#     path:str=None
+
+#     def check_path(self):
+#         assert os.path.exists(self.path), f"Path {self.path} does not exist"
+# TODO later
+
 
 def save_pickle(data_dict, file_name):
     """Save given data dict to pickle file file_name in models/
