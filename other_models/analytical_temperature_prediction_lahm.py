5bedc4a94158618e28821d0f009fd0075a1cd09c
ac130513 <julia.pelzer@ipvs.uni-stuttgart.de>
2023-02-24 13:54:00 +0100

    lahm: savefig

--- other_models/analytical_temperature_prediction_lahm.py
+++ other_models/analytical_temperature_prediction_lahm.py
@@ -100,7 +100,7 @@ def plot_temperature_lahm(data, x_grid, y_grid, title="", ellipses=None):
             plt.contourf(x_grid, y_grid, data, extent=(0,1280,100,0))
         elif index == 2:
             plt.title("Temperature field with focused contour lines [10 °C, 15 °C]")
-            levels = np.arange(10, 15.0, 0.5)
+            levels = np.arange(10, 15.0, 0.25)
             plt.contourf(x_grid, y_grid, data, levels=levels, cmap='RdBu_r', extent=(0,1280,100,0))
 
         if ellipses:
@@ -112,7 +112,8 @@ def plot_temperature_lahm(data, x_grid, y_grid, title="", ellipses=None):
         plt.ylabel("x [m]")
         _aligned_colorbar(label=title)
     plt.xlabel("y [m]")
-    plt.show()
+    # plt.show()
+    plt.savefig(f"{title}.png")
 
 ###### helper functions
 def _time_years_to_seconds(time_years):
