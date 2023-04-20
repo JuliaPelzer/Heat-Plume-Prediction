4c0304c50b513f617f0ffec6257e3f997b10ffdd
JuliaPelzer <jupelzer@outlook.de>
2023-04-20 16:47:29 +0200

    visu: better, temp, clean up contourfargs & faster by not closing pics that often

--- utils/visualize_data.py
+++ utils/visualize_data.py
@@ -6,12 +6,15 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import torch
 import logging
 from typing import Dict
+from line_profiler_decorator import profiler
 
 from torch.utils.data import DataLoader
 from networks.unet import UNet
 
-# TODO: look at vispy library for plotting 3D data
+import matplotlib as mpl
+mpl.rcParams.update({'figure.max_open_warning': 0})
 
+# TODO: look at vispy library for plotting 3D data
 
 @dataclass
 class DataToVisualize:
@@ -19,16 +22,24 @@ class DataToVisualize:
     name: str
     extent_highs :tuple = (1280,100) # x,y in meters
     imshowargs: Dict = field(default_factory=dict)
+    contourfargs: Dict = field(default_factory=dict)
     contourargs: Dict = field(default_factory=dict)
 
     def __post_init__(self):
-        # TODO: add reasonable extent, make variable
         extent = (0,int(self.extent_highs[0]),int(self.extent_highs[1]),0)
-        self.imshowargs["extent"]  = extent
-        self.contourargs["extent"] = extent
-        self.contourargs = {"levels": np.arange(
-            10.4, 15.6, 0.25), "cmap": "RdBu_r", }
 
+        self.imshowargs = {"cmap": "RdBu_r", 
+                           "extent": extent}
+
+        self.contourfargs = {"levels": np.arange(10.4, 15.6, 0.25), 
+                             "cmap": "RdBu_r", 
+                             "extent": extent}
+        
+        T_gwf = 10.6
+        T_inj_diff = 5.0
+        self.contourargs = {"levels" : [np.round(T_gwf + 1, 1), np.round(T_gwf + T_inj_diff, 1)],
+                            "cmap" : "Pastel1", 
+                            "extent": extent}
 
 def plot_sample(model: UNet, dataloader: DataLoader, device: str, amount_plots: int = inf, plot_name: str = "default"):
 
@@ -79,10 +90,15 @@ def plot_sample(model: UNet, dataloader: DataLoader, device: str, amount_plots: 
                     x[index], physical_var,extent_highs)
 
             name_pic = f"runs/{plot_name}_{current_id}"
-            _plot_datafields(dict_to_plot, name_pic=name_pic)
-            _plot_isolines(dict_to_plot, name_pic=name_pic)
+            figsize_x = extent_highs[0]/extent_highs[1]*3
+            _plot_datafields(dict_to_plot, name_pic=name_pic, figsize_x=figsize_x)
+            _plot_isolines(dict_to_plot, name_pic=name_pic, figsize_x=figsize_x)
+            # _plot_temperature_field(dict_to_plot, name_pic=name_pic, figsize_x=figsize_x)
 
             logging.info(f"Resulting pictures are at runs/{plot_name}_*")
+
+            # if (current_id > 0 and current_id % 6 == 0) or current_id >= amount_plots-1:
+            #     plt.close("all")
 
             if current_id >= amount_plots-1:
                 max_error = np.max(error[-1].cpu().numpy())
@@ -90,41 +106,70 @@ def plot_sample(model: UNet, dataloader: DataLoader, device: str, amount_plots: 
                 return error_mean, max_error
             current_id += 1
 
-
-def _plot_datafields(data: Dict[str, DataToVisualize], name_pic: str):
+# @profiler        
+def _plot_datafields(data: Dict[str, DataToVisualize], name_pic: str, figsize_x: float = 38.4):
     n_subplots = len(data)
     _, axes = plt.subplots(n_subplots, 1, sharex=True,
-                           figsize=(20, 3*(n_subplots)))
+                           figsize=(figsize_x, 3*(n_subplots)))
 
-    for index, data_point in enumerate(data.values()):
+    for index, (name, datapoint) in enumerate(data.items()):
         plt.sca(axes[index])
-        plt.imshow(data_point.data.T, **data_point.imshowargs)
+        if name in ["t_true", "t_out", "error"]:  
+            if name=="error": datapoint.contourargs["levels"] = [level - 10.6 for level in datapoint.contourargs["levels"]] 
+            CS = plt.contour(torch.flip(datapoint.data, dims=[1]).T, **datapoint.contourargs)
+            plt.clabel(CS, inline=1, fontsize=10)
+
+        plt.imshow(datapoint.data.T, **datapoint.imshowargs)
         plt.gca().invert_yaxis()
 
         plt.ylabel("x [m]")
         plt.xlabel("y [m]")
-        _aligned_colorbar(label=data_point.name)
+        _aligned_colorbar(label=datapoint.name)
 
-    plt.suptitle("Datafields: Input, Output, Error")
+    plt.suptitle("Datafields: Inputs, Output, Error")
     plt.savefig(f"{name_pic}.png")
-    plt.close()
+    plt.savefig(f"{name_pic}.svg")
 
-def _plot_isolines(data: Dict[str, DataToVisualize], name_pic: str):
+def _plot_isolines(data: Dict[str, DataToVisualize], name_pic: str, figsize_x: float = 38.4):
     # helper function to plot isolines of temperature out
-    _, axis = plt.subplots(figsize=(20, 3)) #TODO (20,5)
-    plt.sca(axis)
-    data_point = data["t_out"]
-    plt.contourf(data_point.data.T, **data_point.contourargs)
-    plt.gca().invert_yaxis()
-    plt.ylabel("x [m]")
-    plt.xlabel("y [m]")
-    _aligned_colorbar()
+    _, axes = plt.subplots(2, 1, sharex=True, figsize=(figsize_x, 3*2))
+
+    for index, name in enumerate(["t_true", "t_out"]):
+        plt.sca(axes[index])
+        datapoint = data[name]
+        datapoint.data = torch.flip(datapoint.data, dims=[1])
+        plt.contourf(datapoint.data.T, **datapoint.contourfargs)
+        plt.ylabel("x [m]")
+        plt.xlabel("y [m]")
+        _aligned_colorbar(label=datapoint.name)
 
     plt.suptitle(f"Isolines of Temperature [°C]")
     plt.savefig(f"{name_pic}_isolines.png")
     # plt.savefig(f"{name_pic}.svg")
-    plt.close()
 
+def _plot_temperature_field(data: Dict[str, DataToVisualize], name_pic:str, figsize_x: float = 38.4):
+    """
+    Plot the temperature field. 
+    almost-copy of other_models/analytical_models/utils_and_visu
+    """
+    _, axes = plt.subplots(3,1,sharex=True,figsize=(figsize_x, 3*3))
+    
+    for index, name in enumerate(["t_true", "t_out", "error"]):
+            plt.sca(axes[index])
+            datapoint = data[name]
+            if name=="error": datapoint.contourargs["levels"] = [level - 10.6 for level in datapoint.contourargs["levels"]]
+            CS = plt.contour(torch.flip(datapoint.data, dims=[1]).T, **datapoint.contourargs)
+            plt.clabel(CS, inline=1, fontsize=10)
+            plt.imshow(datapoint.data.T, **datapoint.imshowargs)
+            plt.gca().invert_yaxis()
+            plt.xlabel("y [m]")
+            plt.ylabel("x [m]")
+            _aligned_colorbar(label=datapoint.name)
+
+    T_gwf_plus1, T_gwf_plusdiff = datapoint.contourargs["levels"]
+    plt.suptitle(f"Temperature field and isolines of {T_gwf_plus1} and {T_gwf_plusdiff} °C")
+    plt.savefig(f"{name_pic}_combined.png")
+    plt.savefig(f"{name_pic}_combined.svg")
 
 def _aligned_colorbar(*args, **kwargs):
     cax = make_axes_locatable(plt.gca()).append_axes(
