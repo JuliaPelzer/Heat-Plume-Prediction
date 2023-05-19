065dc21d9fa1b3fe0717f7466fee168eb561d7af
ac130513 <julia.pelzer@ipvs.uni-stuttgart.de>
2023-05-19 16:21:02 +0200

    pytest SDF

--- unittests/test_pytest.py
+++ unittests/test_pytest.py
@@ -1,132 +1,61 @@
 import pytest
-import os
-import data.transforms as trans    # The code to test
-import data.dataloader as dataloader
-import data.dataset_loading as lp
-import data.utils as utils
+import torch
 import networks.losses
-
-# additional libraries
-import numpy as np
-from torch import Tensor, zeros, eq, Size, mean, std
+from data.transforms import ComposeTransform, SignedDistanceTransform
 
 def test_sdf_transform():
-    # TODO
-    pass
-    # # Fixture
-    # testor = Tensor(np.array([[[1,2,3],[100,5,6]]]))
-    # data = utils.PhysicalVariables(time="now", properties=["Material ID"])
-    # data["Material ID"] = testor
-    # # Expected result
-    # sdf = Tensor(np.array([[[1,1.4142,2.2361],[0,1,2]]]))
-    # # Actual result
-    # sdf_transform = trans.SignedDistanceTransform()
-    # actual_result = sdf_transform(data)
-    # # Test
-    # assert eq(actual_result["Material ID"].value, sdf).flatten
+    trafo = ComposeTransform([SignedDistanceTransform()])
+    # Fixture
+    data = {"SDF": torch.zeros((9, 9, 2))}
+    data["SDF"][1,1,0] = 1
+    # Expected result
+    expected = torch.Tensor([
+        [[0.8579, 0.8259], [0.8995, 0.8579], [0.8579, 0.8259], [0.7753, 0.7538], [0.6822, 0.6667], [0.5856, 0.5736], [0.4875, 0.4778], [0.3887, 0.3805], [0.2893, 0.2823]],
+        [[0.8995, 0.8579], [1.0000, 0.8995], [0.8995, 0.8579], [0.7990, 0.7753], [0.6985, 0.6822], [0.5980, 0.5856], [0.4975, 0.4875], [0.3970, 0.3887], [0.2965, 0.2893]],
+        [[0.8579, 0.8259], [0.8995, 0.8579], [0.8579, 0.8259], [0.7753, 0.7538], [0.6822, 0.6667], [0.5856, 0.5736], [0.4875, 0.4778], [0.3887, 0.3805], [0.2893, 0.2823]],
+        [[0.7753, 0.7538], [0.7990, 0.7753], [0.7753, 0.7538], [0.7157, 0.6985], [0.6376, 0.6239], [0.5505, 0.5394], [0.4588, 0.4495], [0.3644, 0.3565], [0.2683, 0.2615]],
+        [[0.6822, 0.6667], [0.6985, 0.6822], [0.6822, 0.6667], [0.6376, 0.6239], [0.5736, 0.5619], [0.4975, 0.4875], [0.4140, 0.4054], [0.3258, 0.3184], [0.2346, 0.2280]],
+        [[0.5856, 0.5736], [0.5980, 0.5856], [0.5856, 0.5736], [0.5505, 0.5394], [0.4975, 0.4875], [0.4315, 0.4226], [0.3565, 0.3487], [0.2753, 0.2683], [0.1897, 0.1835]],
+        [[0.4875, 0.4778], [0.4975, 0.4875], [0.4875, 0.4778], [0.4588, 0.4495], [0.4140, 0.4054], [0.3565, 0.3487], [0.2893, 0.2823], [0.2150, 0.2086], [0.1354, 0.1296]],
+        [[0.3887, 0.3805], [0.3970, 0.3887], [0.3887, 0.3805], [0.3644, 0.3565], [0.3258, 0.3184], [0.2753, 0.2683], [0.2150, 0.2086], [0.1472, 0.1413], [0.0734, 0.0680]],
+        [[0.2893, 0.2823], [0.2965, 0.2893], [0.2893, 0.2823], [0.2683, 0.2615], [0.2346, 0.2280], [0.1897, 0.1835], [0.1354, 0.1296], [0.0734, 0.0680], [0.0051, 0.0000]]])
+    # Actual result
+    data = trafo(data)
+    # Test
+    assert torch.allclose(data["SDF"], expected, atol=1e-4)
+    assert data["SDF"][1,1,0] == 1
 
-def test_normalize_transform():
-    # TODO
-    pass
-    # data = utils.PhysicalVariables(time="now", properties=["test"])
-    # data["test"] = Tensor(np.array([[[[-2, -1, -1],[-1, 0, -1], [-1, -1, -1]]]]))
-    # mean_val = {"test": -1}
-    # std_val = {"test": 0.5}
-    # data_norm = Tensor(np.array([[[[-2, 0, 0],[0, 2, 0], [0, 0, 0]]]]))
-    # transform = trans.NormalizeTransform()
-    # tensor_eq = eq(transform(data, mean_val, std_val)["test"].value, data_norm).flatten
-    # assert tensor_eq
+# def test_normalize_transform():
+#     # TODO
+#     pass
+#     # data = utils.PhysicalVariables(time="now", properties=["test"])
+#     # data["test"] = Tensor(np.array([[[[-2, -1, -1],[-1, 0, -1], [-1, -1, -1]]]]))
+#     # mean_val = {"test": -1}
+#     # std_val = {"test": 0.5}
+#     # data_norm = Tensor(np.array([[[[-2, 0, 0],[0, 2, 0], [0, 0, 0]]]]))
+#     # transform = trans.NormalizeTransform()
+#     # tensor_eq = eq(transform(data, mean_val, std_val)["test"].value, data_norm).flatten
+#     # assert tensor_eq
 
-def test_reduceto2d_transform():
-    # TODO
-    pass
-    # data = utils.PhysicalVariables(time="now", properties=["test"])
-    # data["test"] = zeros([4,2,4])
-    # data_reduced = zeros([1,2,4])
-    # # Actual result
-    # data_actual = trans.ReduceTo2DTransform()(data, loc_hp=[0,1,1])
-    # # Test
-    # assert data_actual["test"].value.shape == data_reduced.shape
-    # tensor_eq = eq(data_actual["test"].value, data_reduced).flatten
-    # assert tensor_eq
+# def test_reduceto2d_transform():
+#     # TODO
+#     pass
+#     # data = utils.PhysicalVariables(time="now", properties=["test"])
+#     # data["test"] = zeros([4,2,4])
+#     # data_reduced = zeros([1,2,4])
+#     # # Actual result
+#     # data_actual = trans.ReduceTo2DTransform()(data, loc_hp=[0,1,1])
+#     # # Test
+#     # assert data_actual["test"].value.shape == data_reduced.shape
+#     # tensor_eq = eq(data_actual["test"].value, data_reduced).flatten
+#     # assert tensor_eq
 
-# def test_data_init():
-#     name_folder_destination = "test_bm"
-#     try:
-#         os.mkdir(os.path.join(os.getcwd(), "runs", name_folder_destination))
-#     except:
-#         pass
+# def test_visualize_data():
+#     pass
 
-#     # _, dataloaders = lp.init_data(reduce_to_2D=False, dataset_name="test_dataset_01", inputs="xyz", name_folder_destination=name_folder_destination)
-#     # assert len(dataloaders) == 2
-#     # assert len(dataloaders["train"].dataset[0].inputs) == 4
-#     # for prop in dataloaders["train"].dataset[0].inputs.values():
-#     #     assert prop.shape() == (8,128,8)
-#     #     break
-
-#     _, dataloaders = lp.init_data(reduce_to_2D=True, dataset_name="test_dataset_bm_10", inputs="pk", name_folder_destination=name_folder_destination)
-#     assert len(dataloaders) == 2
-#     assert len(dataloaders["train"].dataset[0].inputs) == 3
-#     for prop in dataloaders["train"].dataset[0].inputs.values():
-#         assert prop.shape() == (8,128,8)
-#         break
-
-#     _, dataloaders = lp.init_data(reduce_to_2D=True, dataset_name="test_dataset_bm_01", inputs="pk", name_folder_destination=name_folder_destination)
-#     for prop in dataloaders["train"].dataset[0].inputs.values():
-#         assert prop.shape() == (128,32)
-#         break
-    
-#     _, dataloaders = lp.init_data(reduce_to_2D=False, dataset_name="test_dataset_bm_01", inputs="p", name_folder_destination=name_folder_destination)
-#     assert len(dataloaders["train"].dataset[0].inputs) == 2
-#     for prop in dataloaders["train"].dataset[0].inputs.values():
-#         assert prop.shape() == (8,128,8)
-#         break
-    
-#     _, dataloaders = lp.init_data(reduce_to_2D=False, dataset_name="test_dataset_bm_01", inputs="", name_folder_destination=name_folder_destination)
-#     assert len(dataloaders["train"].dataset[0].inputs) == 1
-#     assert dataloaders["train"].dataset[0].inputs.get_ids_list() == ["Material ID"] or dataloaders["train"].dataset[0].inputs.get_ids_list() == ["Material ID"]
-#     for prop in dataloaders["train"].dataset[0].inputs.values():
-#         assert prop.shape() == (8,128,8)
-#         break
-
-#     path = os.path.join("runs", name_folder_destination)
-#     os.system(f"rm -r {path}")
-
-# def test_combinations():
-#     datasets, dataloaders = lp.init_data(dataset_name="test_dataset_bm_10", inputs="xyz")
-#     assert len(datasets["train"])+len(datasets["val"])+len(datasets["test"])==10, "number of runs in datasets should be 10"
-#     assert dataloaders["train"].dataset[0], "getitem/ load datapoint does not really work in combination with init_data"
-#     assert datasets["train"][0].inputs["Liquid X-Velocity [m_per_y]"].__repr__() == "Liquid X-Velocity (in m_per_y) with (128, 8) elements", "combination of data_init and repr of PhysicalVariable does not work"
-#     datasets, dataloaders = lp.init_data(reduce_to_2D=False, dataset_name="test_dataset_bm_10", inputs="xyz")
-#     assert datasets["train"][0].inputs["Liquid X-Velocity [m_per_y]"].__repr__() == "Liquid X-Velocity (in m_per_y) with (8, 128, 8) elements", "combination of data_init and repr of PhysicalVariable does not work"
-#     assert not datasets["train"][0].inputs["Liquid X-Velocity [m_per_y]"] == datasets["train"][1].inputs["Liquid X-Velocity [m_per_y]"], "same values in two datapoints?! that should not happen - problem with copying"
-
-# def test_dataloader_iter():
-#     name_folder_destination = "test_bm"
-#     try:
-#         os.mkdir(os.path.join(os.getcwd(), "runs", name_folder_destination))
-#     except:
-#         pass
-
-#     _, dataloaders = lp.init_data(dataset_name="test_dataset_bm_10", reduce_to_2D=True, batch_size=4, name_folder_destination=name_folder_destination)
-#     sizes_x = []
-#     sizes_y = []
-#     for dataloader in dataloaders.values():
-#         for _, datapoint in enumerate(dataloader):
-#             x = datapoint.inputs.float()
-#             y = datapoint.labels.float()
-#             sizes_x.append(Tensor.size(x))
-#             sizes_y.append(Tensor.size(y))
-#     assert(sizes_x == [Size([4, 6, 8, 128, 8]), Size([3, 6, 8, 128, 8]), Size([2, 6, 8, 128, 8]), Size([1, 6, 8, 128, 8])]), "enumerate(dataloader) does not work properly"
-#     assert(sizes_y == [Size([4, 4, 8, 128, 8]), Size([3, 4, 8, 128, 8]), Size([2, 4, 8, 128, 8]), Size([1, 4, 8, 128, 8])]), "enumerate(dataloader) does not work properly"
-#     # first number depends on split + batch size ´-> check whether split changed
-
-def test_visualize_data():
-    pass
-
-def test_train_model():
-    pass
-    """ Test input format etc. of train_model"""
+# def test_train_model():
+#     pass
+#     """ Test input format etc. of train_model"""
 
 def test_mselossexcludenotchangedtemp():
     import torch
@@ -141,5 +70,5 @@ def test_mselossexcludenotchangedtemp():
     assert expected_value == pytest.approx(value)
 
 
-# if __name__ == "__main__":
-#     test_data_init()
+if __name__ == "__main__":
+    test_sdf_transform()
