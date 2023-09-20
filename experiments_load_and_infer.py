f6f0b7e2dd7eef7496e9d327afe00af8053c87ba
JuliaPelzer <jupelzer@outlook.de>
2023-09-20 13:42:30 +0200

    experiments: also for 2hp

--- experiments_load_and_infer.py
+++ experiments_load_and_infer.py
@@ -54,7 +54,7 @@ def run(settings: SettingsTraining):
 def load_and_infer(settings: SettingsTraining, datapoint_name:str, model_path: str, device: str = "cuda:3"):
     start_time = time.perf_counter()
     datapoint = torch.load(os.path.join(settings.datasets_path, settings.dataset_name, "Inputs", datapoint_name))
-    datapoint = torch.unsqueeze(datapoint, 0).to(device)
+    datapoint = torch.unsqueeze(torch.Tensor(datapoint), 0).to(device)
 
     model = load_model({"model_choice": settings.model_choice, "in_channels": len(settings.inputs_prep)}, model_path, "model", device)
     model.to(device)
@@ -77,27 +77,32 @@ if __name__ == "__main__":
     parser.add_argument("--model_choice", type=str, default="unet")
     parser.add_argument("--inputs_prep", type=str, default="pksi")
     parser.add_argument("--name_extension", type=str, default="")
+    parser.add_argument("--case_2hp", type=bool, default=False)
     args = parser.parse_args()
 
     args.name_folder_destination = "experiments"+args.path_to_model[7:]
     
-    default_raw_dir, datasets_prepared_dir, dataset_prepared_full_path = set_paths(args.dataset_name, args.inputs_prep, args.name_extension)
+    default_raw_dir, datasets_prepared_dir, dataset_prepared_full_path = set_paths(args.dataset_name, args.inputs_prep, args.name_extension, args.case_2hp)
     args.datasets_path = datasets_prepared_dir
 
     # prepare dataset if not done yet
     if not os.path.exists(dataset_prepared_full_path):
-        args_prep = {"raw_dir": default_raw_dir,
-            "datasets_dir": datasets_prepared_dir,
-            "dataset_name": args.dataset_name,
-            "inputs_prep": args.inputs_prep,
-            "name_extension": args.name_extension}
-        args_prep = SettingsPrepare(**args_prep)
-        prepare_dataset(args=args_prep)
-        print(f"Dataset {dataset_prepared_full_path} prepared")
-
+        if args.case_2hp:
+            raise NotImplementedError("dataset needs to be prepared manually for 2HP case")
+        else:
+            args_prep = {"raw_dir": default_raw_dir,
+                "datasets_dir": datasets_prepared_dir,
+                "dataset_name": args.dataset_name,
+                "inputs_prep": args.inputs_prep,
+                "name_extension": args.name_extension}
+            args_prep = SettingsPrepare(**args_prep)
+            prepare_dataset(args=args_prep)
+            print(f"Dataset {dataset_prepared_full_path} prepared")
     else:
         print(f"Dataset {dataset_prepared_full_path} already prepared")
-    args.dataset_name += "_"+args.inputs_prep + args.name_extension
+
+    if not args.case_2hp:
+        args.dataset_name += "_"+args.inputs_prep + args.name_extension
 
     settings = SettingsTraining(**vars(args))
     if settings.name_folder_destination == "":
