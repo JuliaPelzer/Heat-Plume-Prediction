53641b44d0dd9a1704cab07d267d72c11fa8654b
JohannaHn <st180465@stud.uni-stuttgart.de>
2024-07-10 11:57:34 +0200

    plot entire perm

--- data_stuff/dataset.py
+++ data_stuff/dataset.py
@@ -157,15 +157,13 @@ class DatasetExtendConvLSTM(Dataset):
             self.label_names.append(filename)
         self.input_names.sort()
         self.label_names.sort()
+        print(self.label_names)
         self.spatial_size = torch.load(self.path / "Inputs" / self.input_names[0]).shape[1:]
         self.box_len:int = box_len
         self.skip_per_dir:int = skip_per_dir
         self.dp_per_run:int = (self.spatial_size[0] - box_len) // skip_per_dir
         self.total_time_steps: int = total_time_steps
-        print(f"dp_per_run: {self.dp_per_run}, spatial_size: {self.spatial_size}")
-        print(f"self.path: {self.path}")
-        print(f'self.input_names: {self.input_names}')
-        print(f'self.label')
+       
 
     @property
     def input_channels(self):
@@ -184,33 +182,33 @@ class DatasetExtendConvLSTM(Dataset):
         return len(self.input_names) * self.dp_per_run
     
     
-
     def __getitem__(self,idx):
-        idx = idx % 3
+        #idx = idx % 3
         run_id, window_nr = self.idx_to_window(idx)
-        file_path = self.path / "Inputs" / self.input_names[run_id]
-        if not file_path.exists():
-            raise FileNotFoundError(f"File not found: {file_path}")
-        if file_path.stat().st_size == 0:
-            raise EOFError(f"File is empty: {file_path}")
-        
-        try:
-            input = torch.load(self.path / "Inputs" / self.input_names[run_id])
-            input = input[:, self.skip_per_dir*window_nr:self.skip_per_dir*window_nr+(self.box_len),:]        
-            temp = torch.load(self.path / "Labels" / self.input_names[run_id])
-            temp = temp[:, self.skip_per_dir*window_nr:self.skip_per_dir*window_nr+(self.box_len),:] 
-        except EOFError as e:
-            print(f"Error loading file: {file_path}")
-            raise e
-        except Exception as e:
-            print(f"Unexpected error loading file: {file_path}")
-            raise e
-        
-        inputs, temp = input.unsqueeze(1), temp.unsqueeze(1)
-        inputs = torch.cat((inputs, temp), dim=0)
-        input_slices = torch.tensor_split(inputs,self.total_time_steps,axis=2)
+        file_path_inputs = self.path / "Inputs" / self.input_names[run_id]
+        file_path_labels = self.path / "Labels" / self.input_names[run_id]
+
+        input = torch.load(file_path_inputs)
+        input = input[:, self.skip_per_dir*window_nr:self.skip_per_dir*window_nr+(self.box_len),:]        
+        temp = torch.load(file_path_labels)
+        temp = temp[:, self.skip_per_dir*window_nr:self.skip_per_dir*window_nr+(self.box_len),:] 
+
+        input, temp = input.unsqueeze(1), temp.unsqueeze(1)
+        input = torch.cat((input, temp), dim=0)
+        input_slices = torch.tensor_split(input,self.total_time_steps,axis=2)
         temp_slices = torch.tensor_split(temp, self.total_time_steps, axis=2)
-        input_seq = torch.cat(input_slices, dim=1)[:,:-1]
+        input_seq = torch.cat(input_slices, dim=1)
+        # print('Before________')    
+        # print(f'Shape of input_seq: {input_seq.shape}')
+        # print(f'max of input_seq: {input_seq[-1,-64:].max()}')
+        # print(f'min of input_seq: {input_seq[-1,-64:].min()}')
+
+        # set last temp-square to 0
+        input_seq[-1,-1] = input_seq[-1].mean() #torch.zeros_like(input_seq[0,-64:])
+        # print('After__________')
+        # print(f'Shape of input_seq: {input_seq.shape}')
+        # print(f'max of input_seq: {input_seq.max()}')
+        # print(f'min of input_seq: {input_seq.min()}')
         output = torch.cat(temp_slices, dim=1)[:,-1]  
         
         return input_seq, output
--- postprocessing/visualization.py
+++ postprocessing/visualization.py
@@ -146,11 +146,12 @@ def prepare_data_to_plot_convLSTM(x: torch.Tensor, y: torch.Tensor, y_out:torch.
     # Shape of y_out: torch.Size([64, 64])
     
     perm = x[1]
-    perm = perm.reshape(576,64)
+    perm = perm.reshape(640,64)
     press = x[0]
-    press = press.reshape(576,64)
+    press = press.reshape(640,64)
     temp = x[4]
-    temp = temp.reshape(576,64)
+    temp = temp.reshape(640,64)
+    print(f'shape of temp: {temp.shape}')
     press_max = info['Inputs']['Pressure Gradient [-]']['max']
     press_min = info['Inputs']['Pressure Gradient [-]']['min']
     #y = y.reshape(64,64)
@@ -160,9 +161,9 @@ def prepare_data_to_plot_convLSTM(x: torch.Tensor, y: torch.Tensor, y_out:torch.
     #extent_highs = (np.array(info["CellsSize"][:2]) * x.shape[-2:])
 
     dict_to_plot = {        
-        "press" : DataToVisualize(press, "Input: Pressure Gradient", (0,575,64,0), {"vmax": press_max, "vmin": press_min}),
-        "perm" : DataToVisualize(perm,  "Input: Permeabilität",(0,575,64,0)),
-        "temp" : DataToVisualize(temp, "Input: Temperature", ((0,575,64,0)),{"vmax": temp_max, "vmin": temp_min}),
+        "press" : DataToVisualize(press, "Input: Pressure Gradient", (0,640,64,0), {"vmax": press_max, "vmin": press_min}),
+        "perm" : DataToVisualize(perm,  "Input: Permeabilität",(0,640,64,0)),
+        "temp" : DataToVisualize(temp, "Input: Temperature", ((0,640,64,0)),{"vmax": temp_max, "vmin": temp_min}),
         "t_true": DataToVisualize(y, f"Label: Temperature in [°C]", (576, 640, 64, 0),{"vmax": temp_max, "vmin": temp_min}),
         "t_out": DataToVisualize(y_out, "Prediction: Temperature in [°C]",(575,640,64,0), {"vmax": temp_max, "vmin": temp_min}),
         "error": DataToVisualize(torch.abs(y[-65:]-y_out), "Absolute error in [°C]",(575,640,64,0)),
