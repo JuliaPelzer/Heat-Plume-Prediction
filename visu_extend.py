b10510af0682a5c07d0c1e74c8f5ff96a5f4451c
JohannaHn <st180465@stud.uni-stuttgart.de>
2024-09-04 12:28:21 +0200

    cnn visualization works

--- visu_extend.py
+++ visu_extend.py
@@ -3,20 +3,27 @@ import torch
 import matplotlib.pyplot as plt
 import pathlib
 #from utils.utils_args import load_yaml
+from data_stuff.dataset import Dataset
 from networks.unetVariants import UNetHalfPad2
 
 run_id = 977
 inputs =  torch.load(f"/import/sgs.scratch/hofmanja/datasets_prepared/extend_plumes/ep_medium_1000dp_only_vary_dist inputs_gksi/Inputs/RUN_{run_id}.pt", map_location=torch.device('cpu'))
 labels =  torch.load(f"/import/sgs.scratch/hofmanja/datasets_prepared/extend_plumes/ep_medium_1000dp_only_vary_dist inputs_gksi/Labels/RUN_{run_id}.pt", map_location=torch.device('cpu'))
-model = UNetHalfPad2(5)
-model.load(pathlib.Path("/home/hofmanja/1HP_NN/cnn model/ep_medium_2000dp_vary_values inputs_gksi box128 skip16"))
+model = UNetHalfPad2(4)
+model.load(pathlib.Path("/home/hofmanja/1HP_NN/cnn_model/ep_medium_1000dp_only_vary_dist inputs_sik box128 skip16"))
 
 #info = load_yaml("/home/pelzerja/pelzerja/test_nn/1HP_NN/runs/extend/ep_medium_2000dp_vary_values inputs_gksi box128 skip16/info.yaml")
 #cla = load_yaml("/home/pelzerja/pelzerja/test_nn/1HP_NN/runs/extend/ep_medium_2000dp_vary_values inputs_gksi box128 skip16/command_line_arguments.yaml")
 print(inputs.shape, labels.shape)
 #print(info)
 #print(cla)
+inputs = inputs[1:]
+temp = inputs[0]
+inputs[0] = inputs[1]
+inputs[1] = inputs[2]
+inputs[2] = temp
 
+print(inputs.shape, labels.shape)
 len_box = 128
 skip = 32
 overlap = 46
@@ -31,7 +38,6 @@ for idx in range(0,30):
     concat1 = concat1.unsqueeze(0)
     # print(concat1.shape)
     output1 = model(concat1)
-    print(output1.shape)
     outputs[:,start+len_box+overlap:start+2*len_box-overlap] = output1[0,0,:,:].detach().cpu()
     # actual_len = len_box - 2*overlap
     # labels[:,start+len_box+overlap:start+len_box+overlap+actual_len] = output1[0,0,:,:].detach().cpu()
