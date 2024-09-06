1bf972f8e13cfb1ff93c6e2914f84779897bcfba
XiaoyuZha <xiaoyu.zhang6@googlemail.com>
2024-09-06 11:04:55 +0200

    change branch

--- networks/unet.py
+++ networks/unet.py
@@ -3,7 +3,7 @@ from torch import save, tensor, cat, load, equal
 import pathlib
 
 class UNet(nn.Module):
-    def __init__(self, in_channels=2, out_channels=1, init_features=32, depth=5, kernel_size=5, padding_mode="replicate", dilation = 2):
+    def __init__(self, in_channels=2, out_channels=1, init_features=32, depth=5, kernel_size=3, padding_mode="replicate", dilation = 1):
         super().__init__()
         features = init_features        
         self.encoders = nn.ModuleList()
